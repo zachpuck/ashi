813989a4631d0a55999da382bccc3c3e393552ba
zachpuck <zachpuckett@gmail.com>
2017-07-04 00:23:38 -0700

    add gitattributes file

--- scripts/powershell.js
+++ scripts/powershell.js
@@ -1,7 +1,8 @@
 'use strict';
 
-module.exports = robot => {
-    robot.respond(/powershell/i, res => {
+// Everything in this section is what the bot is listening for
+module.exports = (robot) => {
+    robot.respond(/powershell/i, (res) => {
         res.reply("what powershell script would you like to execute?");
     });
 }

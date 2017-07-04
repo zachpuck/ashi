d07cd14b1fdee48482963052862834dfaf86211b
zachpuck <zachpuckett@gmail.com>
2017-07-04 00:31:57 -0700

    remove unused files

--- scripts/powershell.js
+++ scripts/powershell.js
@@ -3,6 +3,6 @@
 // Everything in this section is what the bot is listening for
 module.exports = (robot) => {
     robot.respond(/powershell/i, (res) => {
-        res.reply("what powershell script would you like to execute?");
+        res.reply('what powershell script would you like to execute?');
     });
 }

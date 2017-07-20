81cec86e90e0dc066851c5a40efa080b93a7ebf1
Zach <zachpuckett@gmail.com>
2017-07-19 23:17:50 -0700

    update pomodoro to include timer (#3)

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -3,5 +3,13 @@
 module.exports = function(robot) {
     robot.hear(/pomodoro/, function(res) {
         res.send('time to start working!');
+        setTimeout(function() { breakTime() }, ((25 * 60) * 1000));
+
+        function breakTime() {
+        res.send('OMG! Time to take a break!');
+        setTimeout(function() { res.send('Break time is over! Type \'Pomodoro\' to keep going!'); }, ((5 * 60) * 1000));
+    }
     });
+
+
 }

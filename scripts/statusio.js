151b8e59742d10926425e96675ce782ee528f5e6
zach puckett <zachpuckett@gmail.com>
2017-07-28 23:33:51 -0700

    add request to list components names

--- scripts/statusio.js
+++ scripts/statusio.js
@@ -1,22 +1,37 @@
 'use strict';
 
-const StatusIOApi = require('statusio').StatusIOApi;
-
-const api = new StatusIOApi('fe95676e-421f-4e55-94e3-aa7c9b291828', 'xDaJahiIQcnyuTXBBKT9+dTnTF0WW5Qn9sXwg+Tb4tSsbY+hj4Y41Ty/5nqEaLgMr/PukhDwwjWwIqsyeH7q2Q==')
-api.setStatusPage('568acbf61ce86b532e000488');
-
-
 module.exports = function(robot) {
     robot.hear(/statusio/, function(res) {
-        res.send(
-            api.status.summary(function(error, data){
-                if(error) {
-                    return data;
-                } else {
-                    return data;
-                }
-            })
-        )
-    })
+        res.send('checking statusio!');
 
+        let statusPageId = process.env.statusIoStatusPageId;
+        let apiId = process.env.statusIoApiId
+        let apiKey = process.env.statusIoApiKey        
+        
+        var request = require('request');
+
+        request({
+        method: 'GET',
+        url: 'https://api.status.io/v2/component/list/' + statusPageId,
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-id': apiId,
+            'x-api-key': apiKey
+        }}, function (error, response, body) {
+        // console.log('Status:', response.statusCode);
+        // console.log('Headers:', JSON.stringify(response.headers));
+        // console.log(JSON.parse(body).result.name);
+            let monitoredItems = [];
+            function getStatus(body) {
+                JSON.parse(body).result.forEach((item) => {
+                monitoredItems.push(item.name);
+                });
+            }
+            getStatus(body);
+            console.log(monitoredItems);
+            res.send('currently monitoring: ' + monitoredItems);
+        });
+    });
 }
+
+
